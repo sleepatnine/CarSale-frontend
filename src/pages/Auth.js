a7b1d24343750bd9fefca6e6e33791ba6fa3ac50
Your Name <you@example.com>
2022-04-08 21:09:15 +0300

    v2

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -48,52 +48,64 @@ const Auth = () => {
 
   return (
     <div className="main">
-      <div>
-        <NavLink to={MARKET_ROUTE}>LOGO</NavLink>
-      </div>
-      <div>
-        <NavLink to={LIKES_ROUTE}>LIKES</NavLink>
-      </div>
       <div className="container">
         <div className="content">
           <div className="image">
+            <div className="logo">
+              <NavLink to={MARKET_ROUTE}>CarSale</NavLink>
+            </div>
+
             <img src="https://a.d-cd.net/twAAAgLKeOA-960.jpg"></img>
             <div className="reg-link">
-                <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся</NavLink>
+              <NavLink to={REGISTRATION_ROUTE}>Зарегестроваться!</NavLink>
             </div>
           </div>
-          
+
           <div className="form">
             <h2 className="title">Авторизация</h2>
             <form onSubmit={onAuth}>
-              <input
-                onChange={(e) => email.onChange(e)}
-                onBlur={(e) => email.onBlur(e)}
-                value={email.value}
-                name="email"
-                type="text"
-                placeholder="Enter email"
-                className="form-input"
-              />
+              <div className="form-group">
+                <img
+                  className="icon"
+                  src="https://img.icons8.com/ios-glyphs/344/contacts.png"
+                ></img>
+                <input
+                  onChange={(e) => email.onChange(e)}
+                  onBlur={(e) => email.onBlur(e)}
+                  value={email.value}
+                  name="email"
+                  type="text"
+                  placeholder="Enter email"
+                  className="form-input"
+                />
+              </div>
               {email.isDirty && email.isEmpty && (
                 <div style={{ color: "red" }}>Поле не заполнено</div>
               )}
               {email.isDirty && email.emailError && (
                 <div style={{ color: "red" }}>Неккоректно введён майл</div>
               )}
-              <input
-                onChange={(e) => password.onChange(e)}
-                onBlur={(e) => password.onBlur(e)}
-                value={password.value}
-                name="password"
-                type="text"
-                placeholder="Enter password"
-                className="form-input"
-              />
+              <div className="form-group">
+                <img
+                  className="icon"
+                  src="https://img.icons8.com/ios-glyphs/344/password--v1.png"
+                ></img>
+                <input
+                  onChange={(e) => password.onChange(e)}
+                  onBlur={(e) => password.onBlur(e)}
+                  value={password.value}
+                  name="password"
+                  type="text"
+                  placeholder="Enter password"
+                  className="form-input"
+                />
+              </div>
               {password.isDirty && password.isEmpty && (
                 <div style={{ color: "red" }}>Поле не заполнено</div>
               )}
-              <button className="login-button" type="submit">Войти</button>
+              <button className="login-button" type="submit">
+                Войти
+              </button>
             </form>
           </div>
         </div>
